2934d1aabbda5e364b20ddf41d3a3ff4da871f0c
itshukychen <itsshukychen@gmail.com>
2026-05-30 06:20:37 -0600

    SHU-34: Add dashboard configuration download API endpoint (#23)

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/api/controller/BaseController.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/api/controller/BaseController.java
@@ -61,5 +61,16 @@ public abstract class BaseController {
         errorResponse.put("message", message);
         return Response.status(428).entity(errorResponse).build();
     }
+
+    /**
+     * Sanitize a filename for use in Content-Disposition header.
+     * Escapes backslashes and double quotes to prevent header injection.
+     * 
+     * @param filename The filename to sanitize
+     * @return Sanitized filename safe for use in attachment headers
+     */
+    protected String sanitizeAttachmentFilename(String filename) {
+        return filename.replace("\\", "\\\\").replace("\"", "\\\"");
+    }
 }
 
